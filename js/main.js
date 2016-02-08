2179e4d993afdbb20eeaa978410d8ded78147f30
darkicex3 <maxime.junique@gmail.com>
2016-02-08 14:52:22 +0100

    hfuyf

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 $(function () {
-        $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+    $('.col-md-4').appendTo('.container');
+
 });
