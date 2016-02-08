422156e0f42df7aff92a2c199630d0e9604fa19a
darkicex3 <maxime.junique@gmail.com>
2016-02-08 15:12:34 +0100

    hfuyf

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-$(function () {
-    (function () {
-        $('.col-md-4').appendTo('.container');
-    })();
-});
+function mon_image() {
+    var retour = $('<img />');
+    retour.attr('src', 'https://pixabay.com/static/uploads/photo/2012/04/10/22/55/chess-26779_640.png');
+    retour.css({
+        width: '10',
+        height: 'auto'
+    })
+}
+
+function ligne() {
+    return $('<td></td>');
+}
+
+function colonne() {
+    return $('<tr></tr>');
+}
+
+$(document).ready(function () {
+    $body.empty();
+
+    $('<table></table>');
+
+    for (var i = 0; i < 8; i++) {
+        var img = mon_image();
+        colonne().html(img);
+        for (var j = 0; j < 8; j++) {
+            ligne().html(img);
+        }
+    }
+})
