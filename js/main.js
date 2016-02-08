4867421ffbb4a7d1b88e2337539ffa7f59474418
darkicex3 <maxime.junique@gmail.com>
2016-02-08 14:40:26 +0100

    hfuyf

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
     (function () {
         for (var i = 0; i < 20; i++) {
-            var big = $('jumbotron');
-            big.appendTo('body');
+            var big = $('.col-md-4');
+            big.appendTo('.container');
         }
     })();
 });
